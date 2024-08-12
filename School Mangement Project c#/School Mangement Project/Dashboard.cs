811c4438424ce3ca93ae19122b817d912a728ac4
mervecakir1 <mervecakirabood@gmail.com>
2024-08-12 17:49:40 +0300

    Update Dashboard.cs

--- School Mangement Project c#/School Mangement Project/Dashboard.cs
+++ School Mangement Project c#/School Mangement Project/Dashboard.cs
@@ -18,8 +18,7 @@ namespace School_Mangement_Project
             InitializeComponent();
         }
 
-        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\abadr\Documents\ITschool.mdf;Integrated Security=True;Connect Timeout=30");   //يتم اضافة الاتصال الخاص بقاعدة البيانات
-        private void closepB_Click(object sender, EventArgs e)
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\abadr\Documents\ITschool.mdf;Integrated Security=True;Connect Timeout=30");   //The connection to the database is added
         {
             Application.Exit();
         }

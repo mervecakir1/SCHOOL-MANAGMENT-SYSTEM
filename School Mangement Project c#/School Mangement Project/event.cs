81b8132c92f094f5ca63b75784156d51614fd0bc
mervecakir1 <mervecakirabood@gmail.com>
2024-08-12 17:40:06 +0300

    Update event.cs

--- School Mangement Project c#/School Mangement Project/event.cs
+++ School Mangement Project c#/School Mangement Project/event.cs
@@ -18,8 +18,7 @@ namespace School_Mangement_Project
             InitializeComponent();
             DisplayEvents();
         }
-        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\abadr\Documents\ITschool.mdf;Integrated Security=True;Connect Timeout=30");    //يتم اضافة الاتصال الخاص بقاعدة البيانات
-        private void DisplayEvents()
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\abadr\Documents\ITschool.mdf;Integrated Security=True;Connect Timeout=30");    
         {
             Con.Open();
             string Query = "select * from eventTB";
